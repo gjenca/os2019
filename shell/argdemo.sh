86b5bbbcc4dfaebd78087eefe85f5c19c0c14523
Gejza Jenca <jenca@comp16.b306>
2019-10-29 09:38:48 +0100

    29.10

---
+++ shell/argdemo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo idem.
+echo Pocet argumentov: $#
+echo argumenty $1 $2 $3 $4
+
--- shell/druhe_mocniny.sh
+++ shell/druhe_mocniny.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-for NUM in $(seq 10)
+if test "$#" -eq 1
+then
+	POKIAL="$1"
+elif test "$#" -eq 0
+then
+	POKIAL=10
+else
+	echo 'prilis vela argumentov'
+	exit 1
+fi
+for NUM in $(seq "$POKIAL")
 do
 	echo "$NUM*$NUM="$(echo $NUM*$NUM | bc)
 done
